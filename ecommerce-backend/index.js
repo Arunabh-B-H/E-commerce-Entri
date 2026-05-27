a6f1dd36ba27046f3a217d7adccca349d86cbf58
Arunabh BH <arunabh2023@gmail.com>
2026-05-28 04:12:11 +0530

    feat: implement auth controllers and routes

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/dbConnection');
+const authRoutes = require('./routes/authenticationRoutes');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 // Load env vars
 dotenv.config();
@@ -15,10 +17,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Basic route
+// Routes
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('E-commerce Backend API is running...');
 });
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
